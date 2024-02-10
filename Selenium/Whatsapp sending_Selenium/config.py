094a736ca3e43c18b030be965e22fbc090edc266
bavidoddi-suresh <suresh@nxtwave.tech>
2024-02-10 19:19:02 +0530

    initial commit

---
+++ Selenium/Whatsapp sending_Selenium/config.py
@@ -0,0 +1,4 @@
+local = {
+    "executable_path" : r"/home/revops/Documents/Selenium/chromedriver-linux64/chromedriver",
+    "userDataDir" : r"/home/revops/Documents/Selenium/Chromedriver_Config/.config/Profiles/profile"
+}
