83c4f580b5b781a72e9dfd1d41669f84eb8e4dae
bavidoddi-suresh <159628295+bavidoddi-suresh@users.noreply.github.com>
2024-02-20 19:30:31 +0530

    Delete Selenium/Whatsapp sending_Selenium directory

--- Selenium/Whatsapp sending_Selenium/config.py
+++
@@ -1,4 +0,0 @@
-local = {
-    "executable_path" : r"/home/revops/Documents/Selenium/chromedriver-linux64/chromedriver",
-    "userDataDir" : r"/home/revops/Documents/Selenium/Chromedriver_Config/.config/Profiles/profile"
-}
